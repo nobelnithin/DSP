97b3b4efc889c62711f1c0304afc5848dce1f4bd
nobelnithin <nithinnithin2812@gmail.com>
2024-11-05 10:56:19 +0530

    first commit

---
+++ fft/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/Admin/esp/v5.2.1/esp-idf/components/bootloader/subproject"
+  "C:/Biostim/DSP/fft/build/bootloader"
+  "C:/Biostim/DSP/fft/build/bootloader-prefix"
+  "C:/Biostim/DSP/fft/build/bootloader-prefix/tmp"
+  "C:/Biostim/DSP/fft/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Biostim/DSP/fft/build/bootloader-prefix/src"
+  "C:/Biostim/DSP/fft/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Biostim/DSP/fft/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Biostim/DSP/fft/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
